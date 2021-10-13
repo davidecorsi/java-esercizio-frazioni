047f4225b46b29022551a9a63c726b2508a0b62f
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 21:51:44 +0200

    risoluzione

--- src/Frazione.java
+++ src/Frazione.java
@@ -2,5 +2,43 @@
  * Create una classe Frazione con 2 costruttori, un metodo somma tra le frazioni ed un metodo toString.
  */
 public class Frazione {
+	
+	int num;
+	int den;
+	
+	public Frazione (int num) {
+		this.num = num;
+		den = 1;
+	}
+	
+	public Frazione(int num, int den) {
+		this.num = num;
+		this.den = den;
+	}
+	
+	public int getNum() {
+		return num;
+	}
 
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getDen() {
+		return den;
+	}
+
+	public void setDen(int den) {
+		this.den = den;
+	}
+	
+	public Frazione somma(Frazione f) {
+		int n = getNum() * f.den + getDen() * this.num;
+		int d = getDen() * f.den;
+		return new Frazione(n, d);
+	}
+
+	public String toString() {
+		return getNum() + "/" + getDen();
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@
  */
 public class Main {
 	public static void main(String[] args) {
+		Frazione frazione1 = new Frazione(3, 4);
+		Frazione frazione2 = new Frazione(5, 2);
+		System.out.println(frazione1.somma(frazione2));
 	}
 }
