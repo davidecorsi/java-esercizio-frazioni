09089a447aaff9fbde46da1ce0495ed921bf9355
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 21:50:41 +0200

    first commit

---
+++ src/Frazione.java
@@ -0,0 +1,6 @@
+/*
+ * Create una classe Frazione con 2 costruttori, un metodo somma tra le frazioni ed un metodo toString.
+ */
+public class Frazione {
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/*
+ * Creare il metodo main dove andare a sommare 2 frazioni e si stampa il risultato.
+ */
+public class Main {
+	public static void main(String[] args) {
+	}
+}
